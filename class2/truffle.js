0b393295f4824f269c7bedc411c4b907719ef3b1
seungwon2 <dandelionsw2@ewhain.net>
2021-03-06 15:28:12 +0900

    :recycle: refactor : refactor config code for use rinkeby and ethereum mainnet

--- class2/truffle.js
+++ class2/truffle.js
@@ -1,50 +1,28 @@
+//1. Initialize `truffle-hdwallet-provider`
 const HDWalletProvider = require("truffle-hdwallet-provider");
-const LoomTruffleProvider = require('loom-truffle-provider');
-const mnemonic = "YOUR MNEMONIC HERE";
-module.exports = {
-    // Object with configuration for each network
-    networks: {
-        //development
-        development: {
-            host: "127.0.0.1",
-            port: 7545,
-            network_id: "*",
-            gas: 9500000
-        },
-        // Configuration for Ethereum Mainnet
-        mainnet: {
-            provider: function() {
-                return new HDWalletProvider(mnemonic, "https://mainnet.infura.io/v3/<YOUR_INFURA_API_KEY>")
-            },
-            network_id: "1" // Match any network id
-        },
-        // Configuration for Rinkeby Metwork
-        rinkeby: {
-            provider: function() {
-                // Setting the provider with the Infura Rinkeby address and Token
-                return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/<YOUR_INFURA_API_KEY>")
-            },
-            network_id: 4
-        },
-        // Configuration for Loom Testnet
-        loom_testnet: {
-            provider: function() {
-                const privateKey = 'YOUR_PRIVATE_KEY';
-                const chainId = 'extdev-plasma-us1';
-                const writeUrl = 'wss://extdev-basechain-us1.dappchains.com/websocket';
-                const readUrl = 'wss://extdev-basechain-us1.dappchains.com/queryws';
-                // TODO: Replace the line below
-                const loomTruffleProvider = new LoomTruffleProvider(chainId, writeUrl, readUrl, privateKey);
-                loomTruffleProvider.createExtraAccountsFromMnemonic(mnemonic, 10);
-                return loomTruffleProvider;
+// Set your own mnemonic here
+const mnemonic = "YOUR_MNEMONIC";
 
-            },
-            network_id: '9545242630824'
-        }
+// Module exports to make this configuration available to Truffle itself
+module.exports = {
+  // Object with configuration for each network
+  networks: {
+    // Configuration for mainnet
+    mainnet: {
+      provider: function () {
+        // Setting the provider with the Infura Rinkeby address and Token
+        return new HDWalletProvider(mnemonic, "https://mainnet.infura.io/v3/YOUR_TOKEN")
+      },
+      network_id: "1"
     },
-    compilers: {
-        solc: {
-            version: "0.4.25"
-        }
+    // Configuration for rinkeby network
+    rinkeby: {
+      // Special function to setup the provider
+      provider: function () {
+        // Setting the provider with the Infura Rinkeby address and Token
+        return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/YOUR_TOKEN")
+      },
+      network_id: 4 //Fill in the `network_id` for the Rinkeby network.
     }
-};
+  }
+}; 
