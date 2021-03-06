56453bab8dbca002d466ff83285c8d5582284ab3
seungwon2 <dandelionsw2@ewhain.net>
2021-03-06 12:22:51 +0900

    :sparkles: feat : add two step test code

--- class2/CryptoZombies.js
+++ class2/CryptoZombies.js
@@ -25,4 +25,22 @@ contract("CryptoZombies", (accounts) => {
             assert.equal(newOwner, bob);
         })
     })
+    context("with the two-step transfer scenario", async () => {
+        it("should approve and then transfer a zombie when the approved address calls transferFrom", async () => {
+            const result = await contractInstance.createRandomZombie(zombieNames[0], {from: alice});
+            const zombieId = result.logs[0].args.zombieId.toNumber();
+            await contractInstance.approve(bob, zombieId, {from: alice});
+            await contractInstance.transferFrom(alice, bob, zombieId, {from: bob});
+            const newOwner = await contractInstance.ownerOf(zombieId);
+            assert.equal(newOwner,bob);
+        })
+        it("should approve and then transfer a zombie when the owner calls transferFrom", async () => {
+            const result = await contractInstance.createRandomZombie(zombieNames[0], {from: alice});
+            const zombieId = result.logs[0].args.zombieId.toNumber();
+            await contractInstance.approve(bob, zombieId, {from: alice});
+            await contractInstance.transferFrom(alice, bob, zombieId, {from: alice});
+            const newOwner = await contractInstance.ownerOf(zombieId);
+            assert.equal(newOwner,bob);
+         })
+    })
 })
